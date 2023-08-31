c75dae4ac3f4ad30bd2899029af583ed5ba58246
Aniforka <dmitrii.gavrilov.dev@gmail.com>
2023-08-31 18:15:32 +0300

    ADD: templates and app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import requests
+from pytube import YouTube
+from flask import Flask, render_template, url_for, redirect, request, jsonify
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/', methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/control/full_update", methods=["GET"])
+def full_update_system():
+    os.system("pacman -Suy")
+
+    return jsonify({'status': 200})
+
+@app.route("/control/reboot", methods=["GET"])
+def reboot_system():
+    os.system("reboot")
+
+    return jsonify({'status': 200})
+
+if __name__ == "__main__":
+    socketio.run(app, host='0.0.0.0', port=7777, allow_unsafe_werkzeug=True) # from 40 to 60000
